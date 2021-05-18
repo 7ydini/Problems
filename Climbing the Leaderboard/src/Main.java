8d24313ae3d38fdaa70e527ec09e41c5b5746817
7ydini <61342487+7ydini@users.noreply.github.com>
2021-05-18 12:19:03 +0300

    Add files via upload

--- Climbing the Leaderboard/src/Main.java
+++ Climbing the Leaderboard/src/Main.java
@@ -1,72 +1,90 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Number of players on the leaderboard >> ");
+        System.out.println("Number of players on the leaderboard.");
         int playersInLeaderboard = Constraint();
-        System.out.println("Input space-separated integers ranked[i], the leaderboard scores in decreasing order>>");
-        int[] ranked = ConstraintArray(playersInLeaderboard, 1);//1 - sorted(fof Leaderboard) other - not sorted(player scores)
+        System.out.println("Input space-separated integers ranked[i], the leaderboard scores in decreasing order.");
+        int[] ranked = ConstraintArray(playersInLeaderboard, 1);//1 - sorted(for Leaderboard) other - not sorted(player scores)
 
         System.out.print("Number games the players plays >> ");
         int gamesPlayersPlays = Constraint();
-        System.out.println("Input space-separated integers player[j], the leaderboard scores in decreasing order >> ");
+        System.out.println("Input space-separated integers player[j], the leaderboard scores in decreasing order.");
         int[] player = ConstraintArray(gamesPlayersPlays, 0);
 
         ClimbingTheLeaderboard(player, ranked);
-
-
     }
 
-    private static void ClimbingTheLeaderboard(int[] player, int[] ranked) {
+    private static void ClimbingTheLeaderboard(int[] playerScores, int[] rankedScores) {
         int top;
-        for (int k : player) {
+        for (int score : playerScores) {
             top = 1;
-            for (int i = 0; i < ranked.length; i++, top++) {
-                if (k >= ranked[i]) {
+            for (int i = 0; i < rankedScores.length; i++, top++) {
+                if (score >= rankedScores[i]) {
                     System.out.println(top);
                     break;
-                } else if (k < ranked[i]) {
-                    while (ranked[i + 1] == ranked[i]) {i++;}
+                } else if (score < rankedScores[i]) {
+                    while ( i < rankedScores.length - 1 && rankedScores[i + 1] == rankedScores[i]) {i++;}
+                    if(i == rankedScores.length - 1){
+                        top++;
+                        System.out.println(top);//processing the last element
+                    }
                 }
             }
         }
     }
 
     private static int Constraint() {
-        Scanner scanner = new Scanner(System.in);
-        int constraint = scanner.nextInt();
+        int constraint = 0;
         while (!(constraint >= 1 && constraint <= (2 * (int)Math.pow(10, 5)))) {
-            System.out.print("Input corrected number >> ");
-            constraint = scanner.nextInt();
+            System.out.print("Input number >> ");
+            try{
+                Scanner scanner = new Scanner(System.in);
+                constraint = scanner.nextInt();
+            }catch (InputMismatchException ex){
+                constraint = -1;
+            }
         }
-        //scanner.close();
         return constraint;
     }
 
     private static int[] ConstraintArray(int size, int mode){
-        Scanner scanner = new Scanner(System.in);
-        int[] constraint;
+
+        int[] constraint = new int[0];
         if (mode == 1){
-            constraint = Arrays.stream(scanner.nextLine().split(" ")).sorted().sorted(Comparator.reverseOrder()).mapToInt(Integer::parseInt).toArray();
-        }else constraint = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
         while (constraint.length != size || !CorrectArray(constraint)){
-            System.out.print("Input corrected scores >> ");
-            if (mode == 1){
-                constraint = Arrays.stream(scanner.nextLine().split(" ")).sorted().sorted(Comparator.reverseOrder()).mapToInt(Integer::parseInt).toArray();
-            }else constraint = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            try {
+                System.out.print("Input scores >> ");
+                Scanner scanner = new Scanner(System.in);
+                constraint = Arrays.stream(Arrays.stream(scanner.nextLine().split(" "))
+                        .sorted().mapToInt(Integer::parseInt).toArray())//Sorted Input to array
+                        .boxed().sorted(Collections.reverseOrder())//sorted Integer
+                        .mapToInt(Integer::intValue).toArray();//return int[]
+            }catch (NumberFormatException ex){
+                constraint = new int[0];
+            }
         }
-
+        }else {
+            while (constraint.length != size || !CorrectArray(constraint)) {
+                try {
+                    System.out.print("Input scores >> ");
+                    Scanner scanner = new Scanner(System.in);
+                    constraint = Arrays.stream(scanner.nextLine().split(" "))
+                            .mapToInt(Integer::parseInt).toArray();
+                }catch (NumberFormatException ex){
+                    constraint = new int[0];
+                }
+            }
+        }
         return constraint;
     }
 
     private static boolean CorrectArray(int[] constraint) {
         for (int score:
              constraint) {
-            if(score > (int) Math.pow(10, 9) || score <= 0){
+            if(score > (int) Math.pow(10, 9) || score < 0){
                 return false;
             }
         }
