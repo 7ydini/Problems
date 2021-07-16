c095566e3177f4ca9bb26ceaced6ebff54c8c357
7ydini <61342487+7ydini@users.noreply.github.com>
2021-07-16 10:29:00 +0300

    Create Solution.java

---
+++ Append and Delete/Solution.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     *https://www.hackerrank.com/challenges/append-and-delete/problem
+     * Complete the 'appendAndDelete' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. STRING t
+     *  3. INTEGER k
+     */
+
+    public static String appendAndDelete(String s, String t, int k) {
+    // Write your code here
+        int similarSymbols = 0;
+        while (similarSymbols < s.length() && similarSymbols < t.length() && s.substring(0, similarSymbols).equals(t.substring(0, similarSymbols))) {
+            similarSymbols++;
+        }
+        if(s.length() - similarSymbols + 1 + t.length() - similarSymbols + 1 <= k){
+            return "Yes";
+        }else return "No";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String t = bufferedReader.readLine();
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.appendAndDelete(s, t, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
